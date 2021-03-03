3e2f60da056a1fc493ae7a1a4871374c6b6739c9
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-03 11:53:04 -0300

    create routes for fake email service

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { SenMailController } from "./controllers/SenMailController";
 import SurveyController from "./controllers/SurveyController";
 import { UserController } from "./controllers/UserController";
 
@@ -6,10 +7,12 @@ const router = Router();
 
 const userController = new UserController()
 const surveyController = new SurveyController()
+const surveysUsersController = new SenMailController()
 
 router
     .post('/users', userController.create)
     .post('/surveys', surveyController.create)
     .get('/surveys', surveyController.show)
+    .post('/sendMail', surveysUsersController.execute)
 
 export { router }
