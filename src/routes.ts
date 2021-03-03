6349b08df5a842566e6dd2a1c153b65bc8be94a7
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-03 15:38:10 -0300

    create nps service

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { AnswerController } from "./controllers/AnswerController";
+import { NPSController } from "./controllers/NPSController";
 import { SenMailController } from "./controllers/SenMailController";
 import SurveyController from "./controllers/SurveyController";
 import { UserController } from "./controllers/UserController";
@@ -8,11 +10,15 @@ const router = Router();
 const userController = new UserController()
 const surveyController = new SurveyController()
 const surveysUsersController = new SenMailController()
+const answerController = new AnswerController()
+const npsController = new NPSController()
 
 router
     .post('/users', userController.create)
     .post('/surveys', surveyController.create)
     .get('/surveys', surveyController.show)
     .post('/sendMail', surveysUsersController.execute)
+    .get('/answers/:value', answerController.execute)
+    .get('/nps/:survey_id', npsController.execute)
 
 export { router }
