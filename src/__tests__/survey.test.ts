5b8d05c108d9bb6d2285d9ab04091558fe5f6a36
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-03 00:03:06 -0300

    add some jest tests

---
+++ src/__tests__/survey.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest'
+import { app } from '../app'
+
+import createConnection from '../database'
+
+describe('Surveys', () => {
+
+    beforeAll(async () => {
+        const connection = await createConnection()
+        await connection.runMigrations()
+    })
+
+    it("Should be able to create a new survey", async () => {
+        const response = await request(app).post('/surveys')
+            .send({
+                title: 'Survey Title',
+                description: 'Survey Description'
+            })
+
+        expect(response.status).toBe(201)
+        expect(response.body).toHaveProperty('id')
+    })
+
+    it("Should be able to get all surveys", async () => {
+        await request(app).post('/surveys')
+            .send({
+                title: 'Survey Title part 2',
+                description: 'Survey Description part 2'
+            })
+        const response = await request(app).get('/surveys')
+
+        expect(response.body.length).toBe(2);
+    })
+
+})
